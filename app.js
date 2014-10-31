ce0d89045442bd8265b7d0414c50b128b55c3f78
rtmalone <rtylermalone@gmail.com>
2014-10-30 23:20:39 -0500

    gaaaaahhhhhhh

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 var http = require('http');
-var connect = require
+var connect = require('connect');
+var connect = require('serve-static');
+var morgan = require('morgan');
+
+var port = process.env.PORT || 5050;
+
+var app = connect();
+
+app.use(morgan('dev'));
+app.use(serveStatic(__dirname + 'public'));
+
+http.createServer(app).listen(port, function(){
+  console.log('Node server listening. Port: ' + port + ', Directory: ' + directory);
+});
